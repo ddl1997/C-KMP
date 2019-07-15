d8b363f052cfc1de9d87d42671b3f803bf37c2fe
ddl <dingdonglai@custouch.com>
2019-07-15 17:51:16 +0800

    first

---
+++ KMP_Impl.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMP
+{
+    class KMP_Impl
+    {
+        public int[] _next { get; }
+        string _mod;
+
+        public KMP_Impl(string mod)
+        {
+            _next = new int[mod.Length];
+            _mod = mod;
+            for (int i = 0; i < _next.Length; i++)
+            {
+                if (i == 0)
+                    _next[i] = -1;
+                else
+                {
+                    _next[i] = PrePostMaxLength(mod.Substring(i));
+                }
+            }
+        }
+
+        public int GetIndex(string str)
+        {
+            int matchIndex = -1;
+            int index = 0;
+            int modIndex = 0;
+
+            while (index <= str.Length - _mod.Length)
+            {
+                for (; modIndex < _mod.Length; modIndex++, index++)
+                {
+                    if (!Equals(_mod.ElementAt(modIndex), str.ElementAt(index)))
+                    {
+                        break;
+                    }
+                }
+                if (modIndex == _mod.Length)
+                {
+                    matchIndex = index - _mod.Length;
+                    break;
+                }
+                else if (modIndex == 0)
+                {
+                    modIndex = 0;
+                    index++;
+                }
+                else
+                {
+                    modIndex = _next[modIndex];
+                }
+            }
+
+            return matchIndex;
+        }
+
+        public static int PrePostMaxLength(string str)
+        {
+            int max_length = 0;
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (string.Equals(str.Substring(0, i), str.Substring(str.Length - i, i)))
+                    max_length = i;
+            }
+            return max_length;
+        }
+    }
+}
